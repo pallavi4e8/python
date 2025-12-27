21abd48aa809d9dcbf604ccee54cdf94292d49de
pallavi4e8 <kasireddypallavi1974@gmail.com>
2025-12-27 12:24:57 +0530

    Add files via upload

---
+++ leetcode189.py
@@ -0,0 +1,11 @@
+  def reve(l,r):
+            while l<r:
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+                r-=1
+        reve(0,len(nums)-1)
+        reve(0,k-1)
+        reve(k,len(nums)-1)
+
+
+
